24982ecdd57248eed6366d959a90168c96416196
Marcos Bonfadini <mbabonfadini@gmail.com>
2026-01-20 14:44:27 -0300

    Use environment variable for default file extension
    Replaces the hardcoded default extension with a value from the DEFAULT_EXTENSION environment variable, falling back to '.sswweb' if not set. Loads environment variables using python-dotenv.

--- functions/validar_arquivos.py
+++ functions/validar_arquivos.py
@@ -1,6 +1,7 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-
-def validar_arquivos_pasta(downloads_path, default_extension: str = '.sswweb'):
+def validar_arquivos_pasta(downloads_path, default_extension: str = os.getenv('DEFAULT_EXTENSION', '.sswweb')):
     files = set([os.path.join(downloads_path, f) for f in os.listdir(downloads_path) if f.endswith(default_extension)])
     return files
