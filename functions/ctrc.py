d4211b3556a41d9bafb1646a8903cb170fb8f693
Marcos Bonfadini <mbabonfadini@gmail.com>
2026-01-28 10:52:05 -0300

    feat: Implement CTRC data processing, cleaning, payload building, and batch API submission functionalities.

--- functions/ctrc.py
+++ functions/ctrc.py
@@ -44,11 +44,11 @@ def merge_ctrcs(df_file: DataFrame, df_response: DataFrame) -> DataFrame:
     return df
         
 def new_ctrcs(df: DataFrame) -> DataFrame:
-    df_new_registers = df[( (df['Access key'].isnull()) | (df['Access key'] != df['access_key']) ) & (df['Prefix'].notnull())].copy()
+    df_new_registers = df[(df['access_key'].isna()) & (df['Prefix'].notna())].copy()
     return df_new_registers
 
 def old_ctrcs(df: DataFrame) -> DataFrame:
-    df_registered = df[df['id'].notnull()].copy()
+    df_registered = df[df['id'].notna()].copy()
     df_registered_to_update = df_registered[df_registered['Current location description'] != df_registered['current_location']]
     return df_registered_to_update
 
