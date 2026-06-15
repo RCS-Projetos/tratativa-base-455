3a14b28633668372bddfcced2103b297e6d5ba47
Marcos Bonfadini <mbabonfadini@gmail.com>
2026-06-15 15:58:09 -0300

    fix: update dummy parameter value in report arguments for main script

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def executar_automacao(d_inicial: str, d_final: str, documents:str = 'new'):
         {'key': 'basico', 'value': 'N'},
     ]
 
-    report.build_args(args, act='E1', dummy='1780346189899')
+    report.build_args(args, act='E1', dummy='1781549457075')
     report.execute_report(url = 'https://sistema.ssw.inf.br/bin/ssw0230')
     report.process_report()
 
